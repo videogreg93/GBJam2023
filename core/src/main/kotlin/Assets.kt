eab1de21648360edb7991a41640131c2ff78f102
Gregory Fournier <gregorykarlfournier@gmail.com>
2023-09-18 14:40:53 -0400

    sounds.

--- core/src/main/kotlin/Assets.kt
+++ core/src/main/kotlin/Assets.kt
@@ -13,6 +13,7 @@ const val sandy = "enemy/sandy.png"}
     object Fonts {}
 const val head = "head.png"
     object I18n {}
+const val mapBackground = "mapBackground.png"
 const val paletteTest = "paletteTest.png"
     object Player {
 const val bullet = "player/bullet.png"
@@ -25,4 +26,7 @@ const val downToUp = "shaders/downToUp.png"
 const val leftToRight = "shaders/leftToRight.png"
 const val leftToRight2 = "shaders/leftToRight2.png"
 const val out = "shaders/out.png"}
+    object Sounds {
+const val bullet_ogg_sound = "sounds/bullet.ogg"
+const val playerHit_ogg_sound = "sounds/playerHit.ogg"}
 }
